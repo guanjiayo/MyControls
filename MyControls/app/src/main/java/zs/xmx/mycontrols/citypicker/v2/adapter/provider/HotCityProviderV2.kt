bc02c691269176d5e4f1c4ce85d546a8bb11bf5a
moxiaoming <maiqiming487@qq.com>
2024-05-15 23:54:57 +0800

    111

--- MyControls/app/src/main/java/zs/xmx/mycontrols/citypicker/v2/adapter/provider/HotCityProviderV2.kt
+++ MyControls/app/src/main/java/zs/xmx/mycontrols/citypicker/v2/adapter/provider/HotCityProviderV2.kt
@@ -1,13 +1,16 @@
 package zs.xmx.mycontrols.citypicker.v2.adapter.provider
 
 import android.util.Log
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.provider.BaseItemProvider
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
 import zs.xmx.mycontrols.R
 import zs.xmx.mycontrols.citypicker.CityEntity
-import zs.xmx.mycontrols.citypicker.CityPickerEntity
-import zs.xmx.mycontrols.citypicker.v1.adapter.CityPickerAdapter
 import zs.xmx.mycontrols.citypicker.v2.adapter.CityPickerAdapterV2
+
 
 class HotCityProviderV2 : BaseItemProvider<CityEntity>() {
 
@@ -17,8 +20,23 @@ class HotCityProviderV2 : BaseItemProvider<CityEntity>() {
     override fun convert(helper: BaseViewHolder, item: CityEntity) {
         //https://github.com/lvlife/shoppingDemo
         with(helper) {
-            Log.i("TTTT", "item: $item")
+            Log.i("TTTT", "item: ${item.name}")//还是得按我们箱门的结构来做
+            val mRecyclerView = getView<RecyclerView>(R.id.recyclerview)
+            mRecyclerView.layoutManager =
+                LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+            val adapter = GridAdapter()
+            adapter.addData(item)
+            mRecyclerView.adapter = adapter
         }
+
+    }
+
+    inner class GridAdapter :
+        BaseQuickAdapter<CityEntity, BaseViewHolder>(R.layout.item_city_search) {
+        override fun convert(holder: BaseViewHolder, item: CityEntity) {
+            holder.getView<TextView>(R.id.tv_city_name).text = item.name
+        }
+
     }
 
 
